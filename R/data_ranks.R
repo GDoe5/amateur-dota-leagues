f5f17aba4b6d030b141421c51c5b929c020c489e
Choco McInnes <12865776+GDoe5@users.noreply.github.com>
2026-04-16 09:41:14 +0100

    add rank master data

---
+++ R/data_ranks.R
@@ -0,0 +1,31 @@
+ranks <- c(
+  "",
+  list(
+    " " = c("Uncalibrated", "Immortal"),
+    "Divine" = c("Divine 5", "Divine 4", "Divine 3", "Divine 2", "Divine 1"),
+    "Ancient" = c(
+      "Ancient 5",
+      "Ancient 4",
+      "Ancient 3",
+      "Ancient 2",
+      "Ancient 1"
+    ),
+    "Legend" = c("Legend 5", "Legend 4", "Legend 3", "Legend 2", "Legend 1"),
+    "Archon" = c("Archon 5", "Archon 4", "Archon 3", "Archon 2", "Archon 1"),
+    "Crusader" = c(
+      "Crusader 5",
+      "Crusader 4",
+      "Crusader 3",
+      "Crusader 2",
+      "Crusader 1"
+    ),
+    "Guardian" = c(
+      "Guardian 5",
+      "Guardian 4",
+      "Guardian 3",
+      "Guardian 2",
+      "Guardian 1"
+    ),
+    "Herald" = c("Herald 5", "Herald 4", "Herald 3", "Herald 2", "Herald 1")
+  )
+)
